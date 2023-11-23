bfee31866b53f77130692b54fa03125327336229
Seulah Kim <seulahkim@chunjae.co.kr>
2023-11-23 17:31:12 +0900

    vue1-3

--- app.js
+++ app.js
@@ -1,15 +1,38 @@
 const app = Vue.createApp({
     data() {
         return {
+            url : 'http://www.thenetninja.co.uk',
             showBooks : true,
-            title : 'The Final Empire',
-            author : 'Brandon Sanderson',
-            age : 45,
+            books : [
+                {
+                    title : 'name of the wind',
+                    author : 'patrick rothfuss',
+                },
+                {
+                    title : 'the way of kings',
+                    author : 'brandon sanderson',
+                },
+                {
+                    title : 'the final empire',
+                    author : 'brandon sanderson',
+                },
+
+            ]
         }
     },
     methods : {
         toggleShowBooks() {
             this.showBooks = !this.showBooks
+        },
+        handleEvent(e, data) {
+            console.log(e, e.type)
+            if(data){
+                console.log(data);
+            }
+        },
+        handleMousemove(e){
+            this.x = e.offsetX
+            this.y = e.offsetY
         }
     }
 })
