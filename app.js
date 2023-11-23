84b9ae77066bfcf195a4e1b5b8ff5d31fe26378d
Seulah Kim <seulahkim@chunjae.co.kr>
2023-11-23 15:25:39 +0900

    v-show

--- app.js
+++ app.js
@@ -8,9 +8,8 @@ const app = Vue.createApp({
         }
     },
     methods : {
-        changeTitle(title){
-        //    this.title = 'Words of Randiance'
-            this.title = title
+        toggleShowBooks() {
+            this.showBooks = !this.showBooks
         }
     }
 })
